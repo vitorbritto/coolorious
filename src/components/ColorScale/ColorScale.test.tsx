cd63e9b250244ce3208d1c60d7e6e6920a7b90c6
Matheus Chignolli <matheuschignolli@gmail.com>
2025-01-27 11:39:17 -0300

    feat: add button to copy selected color (HEX and RGB)

--- src/components/ColorScale/ColorScale.test.tsx
+++ src/components/ColorScale/ColorScale.test.tsx
@@ -1,6 +1,14 @@
-import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { fireEvent, render, screen } from '@testing-library/react'
 import ColorScale from '.'
+
+const writeText = vi.fn()
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText,
+  },
+});
 
 describe('ColorScale', () => {
   it('should render color picker', () => {
@@ -14,4 +22,23 @@ describe('ColorScale', () => {
 
     expect(screen.getByText('#FFEC70')).toBeDefined()
   })
+
+  it('should copy HEX and RGB initial color value', () => {
+    const hexAndRgbValues = ['#FFEC70', 'RGB(255, 236, 112)'];
+  
+    render(<ColorScale />);
+  
+    hexAndRgbValues.forEach((value) => {
+      expect(screen.getByText(value)).toBeDefined();
+    });
+  
+    const hexCopyButton = screen.getByLabelText('Copiar HEX da cor escolhida');
+    const rgbCopyButton = screen.getByLabelText('Copiar RGB da cor escolhida');
+  
+    fireEvent.click(hexCopyButton);
+    fireEvent.click(rgbCopyButton);
+  
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(hexAndRgbValues[0]);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(hexAndRgbValues[1]);
+  });
 })
